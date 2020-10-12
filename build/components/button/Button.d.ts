ae25dc7274c2d7f91ada80c90e5f5231b076287d
Nikhil Kumaran <nikhil.kumaran.2011@gmail.com>
2020-10-12 18:45:23 +0530

    👌 IMPROVE: Moved build step to postinstall

--- build/components/button/Button.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-export interface ButtonProps {
-    type?: "normal" | "primary" | "subtle" | "warning" | "danger";
-    backgroundColor?: string;
-    label: string;
-    onClick?: () => void;
-    disabled?: boolean;
-}
-export declare const Button: ({ type, backgroundColor, label, disabled, onClick, ...props }: ButtonProps) => JSX.Element;
